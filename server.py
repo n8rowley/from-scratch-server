dcc026aadf352cd9269b8b4cdd76171877f0b8ab
Nate Rowley <n8rowley17@gmail.com>
2022-02-07 22:40:24 -0700

    We have some operational code

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import os
+import re
 
 
 class CS2610Assn1(BaseHTTPRequestHandler):
@@ -13,16 +14,63 @@ class CS2610Assn1(BaseHTTPRequestHandler):
     Replace this pass statement with your own code:
     """
     
-    def do_GET(self):
-        print("forming response...")
-        print(os.listdir())
-        self.send_response(404)
-        self.send_header("nates-header", "You got this info back")
-        self.end_headers()
-        
 
-        print("\nExiting...\n")
-        exit(0)
+    def beginResponse(self, statusCode):
+        self.send_response(statusCode)
+        self.send_header("Connection", "close")
+        self.send_header("Cache-Control", "500")
+
+    def serveFile(self, filePath, statusCode=200):
+        self.beginResponse(statusCode)
+        f = open(filePath, "rb")
+        data = f.read()
+        f.close()
+        self.send_header("Content-Length", str(len(data)))
+
+        ext = filePath.split(".")[-1]
+        self.send_header("Content-Type", "text/html") #TODO: Fix this
+        self.end_headers()
+        self.wfile.write(data)
+
+
+    def sendRedirect(self, redirectLocation):
+        self.beginResponse(301)
+        self.send_header("Location", redirectLocation)
+        self.end_headers()
+
+    def makeDebug(self):
+        pass
+
+    def teapotResponse(self):
+        pass
+
+    def denyAccess(self):
+        pass
+
+    def send404(self):
+        pass
+
+    def inspectPath(self, requestPath):
+        if requestPath == "":
+            self.sendRedirect("index.html")
+        elif os.access(requestPath + ".html", os.R_OK):
+            self.sendRedirect(requestPath + ".html")
+        elif requestPath.startswith("bio"):
+            self.sendRedirect("about.html")
+
+        else:
+            self.send404()
+
+    def do_GET(self):
+        relativePath = self.path[1:]
+        print(relativePath)
+        if os.access(relativePath, os.R_OK):
+            self.serveFile(relativePath)
+        else:
+            self.inspectPath(relativePath)
+
+        # print("\nExiting...\n")
+        # exit(0)
 
 
 
